8bc10169fef7dd45db3da90c4ecced5bc419b9b9
Shivram Sriramulu <89051296+ShivramSriramulu@users.noreply.github.com>
2025-11-12 21:30:26 -0800

    Time: 6 ms (34.3%), Space: 12.3 MB (87.61%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,13 +1,18 @@
 class Solution(object):
     def findTheDifference(self, s, t):
-        c_s = {}
+        count_s = {}
+        count_t = {}
+
         for char in s:
-            c_s[char] = c_s.get(char, 0) + 1
+            count_s[char] = count_s.get(char, 0) + 1
 
-        c_t = {}
+
         for char in t:
-            c_t[char] = c_t.get(char, 0) + 1
+            count_t[char] = count_t.get(char, 0) + 1
 
-        for k, v in c_t.items():
-            if k not in c_s or c_t[k] != c_s.get(k, 0):
+        for k, v in count_t.items():
+            if count_s.get(k, 0) != v:
                 return k
+
+        
+        
