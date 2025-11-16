a372913a1d706d94b45998bdbc814504325459b7
Shivram Sriramulu <89051296+ShivramSriramulu@users.noreply.github.com>
2025-11-15 16:11:45 -0800

    Time: 43 ms (24.08%), Space: 37.5 MB (7.25%) - LeetHub

---
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def findRepeatedDnaSequences(self, s):
+        freq = {}
+        for i in range(len(s) - 9):
+            sub = s[i:i+10]
+            freq[sub] = freq.get(sub,0) + 1
+        result = []
+        for k,v in freq.items():
+            if v > 1:
+                result.append(k)
+        return result
+
+       
+
+        
