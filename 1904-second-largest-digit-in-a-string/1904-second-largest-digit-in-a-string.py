e3df23d56310359f527da4ed9fa2df61bd9c4430
Shivram Sriramulu <89051296+ShivramSriramulu@users.noreply.github.com>
2025-11-17 22:52:09 -0800

    Time: 7 ms (80.08%), Space: 12.3 MB (87.36%) - LeetHub

--- 1904-second-largest-digit-in-a-string/1904-second-largest-digit-in-a-string.py
+++ 1904-second-largest-digit-in-a-string/1904-second-largest-digit-in-a-string.py
@@ -1,17 +1,17 @@
 class Solution(object):
     def secondHighest(self, s):
 
-        freq = {}  # count frequency of only digits
+        freq = {}  
 
         for ch in s:
-            if ch.isdigit():                     # only numeric
-                freq[ch] = freq.get(ch, 0) + 1   # count frequency
+            if ch.isdigit():                     
+                freq[ch] = freq.get(ch, 0) + 1   
 
-        if len(freq) < 2:    # not enough unique digits
+        if len(freq) < 2:    
             return -1
 
-        # Sort by numeric value DESC (ignore frequency)
+       
         sorted_num = sorted(freq.items(), key=lambda x: int(x[0]), reverse=True)
 
-        # Return the digit (key) of the second largest
+        
         return int(sorted_num[1][0])
