8b00101aab349ed891f559a078433f7140e2bf79
Shivram Sriramulu <89051296+ShivramSriramulu@users.noreply.github.com>
2025-12-12 12:16:08 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,8 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        map ={}
-        for i, num in enumerate(nums):
-            comp = target - num
-            if comp in map:
-                return [map[comp],i]
-            map[num] = i
-      
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
