88da87614a4351b4b0c234b91b0f57e0930bf1d6
Shivram Sriramulu <89051296+ShivramSriramulu@users.noreply.github.com>
2026-01-04 18:09:33 -0800

    Time: 4 ms (50.66%), Space: 12.5 MB (27.07%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,18 +1,11 @@
 class Solution(object):
     def findTheDifference(self, s, t):
-        count_s = {}
-        count_t = {}
-
+        freq = {}
         for char in s:
-            count_s[char] = count_s.get(char, 0) + 1
-
-
+            freq[char] = freq.get(char,0) + 1
         for char in t:
-            count_t[char] = count_t.get(char, 0) + 1
-
-        for k, v in count_t.items():
-            if count_s.get(k, 0) != v:
-                return k
-
-        
-        
+            if freq.get(char,0) ==0:
+                return char
+            freq[char]-=1
+       
+       
